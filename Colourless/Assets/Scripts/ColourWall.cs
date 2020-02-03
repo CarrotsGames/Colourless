ba2ee7e923aeb4cc60a59d03ed9eafdefef8667b
Gabriel P <gaberofl42@gmail.com>
2020-02-04 09:38:35 +1100

    Move through colours
    Made it so player can move through colours if he is that colour
    Added colour prefabs
    Added prefab folder
    Changed materials to there colour

--- Colourless/Assets/Scripts/ColourWall.cs
+++ Colourless/Assets/Scripts/ColourWall.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class ColourWall : MonoBehaviour
 {
+    public GameObject Player;
     public string colour;
     public LayerMask mask;
     float dist;
+    private void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
     private void Update()
     {
-        Vector3 fwd = transform.forward;
-        Debug.DrawRay(transform.position, fwd * 50, Color.green);
-        if (Physics.Raycast(transform.position,fwd,10))
+        //RaycastHit hit;
+        //Vector3 fwd = transform.forward;
+        //Debug.DrawRay(transform.position, fwd * 50, Color.green);
+        //if (Physics.Raycast(transform.position,fwd, out hit))
+        //{           
+        //    if(hit.collider.name == transform.gameObject.tag)
+        //    {
+        //        hit.collider.isTrigger = true;
+        //    }
+        if(Player.tag == transform.gameObject.tag)
         {
-            Debug.Log("A");
+            GetComponent<BoxCollider>().isTrigger = true;
         }
+        else
+        {
+            GetComponent<BoxCollider>().isTrigger = false;
+        }
+
     }
+
 }
+

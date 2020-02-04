28425196be01e47e661a2bf61082986179b74b63
Gabriel P <gaberofl42@gmail.com>
2020-02-04 16:06:36 +1100

    Added teleporter script and prefab

--- Colourless/Assets/Scripts/Movement.cs
+++ Colourless/Assets/Scripts/Movement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
-    private CharacterController controller;
+    [HideInInspector]
+    public CharacterController controller;
     public float speed;
     public float jumpVelocity;
     public float gravity;
@@ -17,6 +18,7 @@ public class Movement : MonoBehaviour
     private Vector3 moveDir = Vector3.zero;
     [SerializeField]
     private bool jumping;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -27,12 +29,17 @@ public class Movement : MonoBehaviour
     void Update()
     {
         currentX += Input.GetAxis("Mouse X");
-
         RotatePlayer();
+        PlayerMovement();
+         
+    }
+
+    void PlayerMovement()
+    {
         if (controller.isGrounded)
         {
-            
-            moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));      
+
+            moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
             moveDir = Camera.main.transform.TransformDirection(moveDir);
             moveDir.y = 0;
             moveDir.Normalize();
@@ -49,7 +56,7 @@ public class Movement : MonoBehaviour
 
             moveDir.x = Input.GetAxisRaw("Horizontal") * speed;
             moveDir.z = Input.GetAxisRaw("Vertical") * speed;
-            
+
 
             moveDir = transform.TransformDirection(moveDir);
             jumping = false;
@@ -59,19 +66,18 @@ public class Movement : MonoBehaviour
         {
             controller.Move(Vector3.down * controller.height / 2 * slopeForce * Time.deltaTime);
         }
-        
-          moveDir.y -= gravity * Time.deltaTime;
 
-          controller.Move(moveDir * Time.deltaTime);
-         
+        moveDir.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDir * Time.deltaTime);
     }
-
     void RotatePlayer()
     {
 
         //Rotation
         rotate.x = transform.position.x - Camera.main.transform.position.x;
         rotate.z = transform.position.z - Camera.main.transform.position.z;
+        
         Vector3 Forward = new Vector3(rotate.x, 0.0f, rotate.z);
         Vector3 NewDirection = Vector3.RotateTowards(transform.forward, Forward, 7 * Time.deltaTime, 0);
         transform.rotation = Quaternion.LookRotation(NewDirection);
